d4af44a10051d8fbb3d8fa29833d20d7a464eec1
debi <root@debi>
2011-08-03 20:17:34 +0200

    2-process IPC via tmpfs file, no printf just time
    Msg       Throughput
    245 byte  108 MB/s

--- fork.c
+++ fork.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <sys/time.h>
 
 int main(void)
 {
@@ -14,11 +15,17 @@ int main(void)
   else if (child == 0) {
     int fd = open("/root/tmp/foo", O_RDONLY | O_CREAT);
     char buf[256];
+    unsigned int bytes = 0;
+    struct timeval time0, time1;
+    gettimeofday(&time0, NULL);
     while (1) {
       lseek(fd, 0, SEEK_SET);
-      read(fd, buf, 256);
-      printf(buf);
+      bytes += read(fd, buf, 256);
+      if (bytes >= 1000000000) break;
     }
+    gettimeofday(&time1, NULL);
+    int dt_us = time1.tv_usec - time0.tv_usec + 1000000 * (time1.tv_sec - time0.tv_sec);
+    printf("Data: 1000 MB\nTime: %d us\nRate: %d MB/s\n", dt_us, 1000000000/dt_us);
   }
   // Parent
   else {
