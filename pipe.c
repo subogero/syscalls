0815e4c6c790a592fd858ecfbb2b6ab8ab920a55
debi <root@debi>
2011-08-05 18:03:50 +0200

    pipe does not wait for user input

--- pipe.c
+++ pipe.c
@@ -6,33 +6,33 @@
 
 int main(void)
 {
-  unsigned char c = '0';
-  int fds[2];
+	unsigned char c = 32;
+	int fds[2];
 
-  pipe(fds);
-  pid_t child = fork();
-  
-  switch (child) {
-  case -1: // Fork error
-    perror("fork failed");
-    return 1;
-  case  0: // Child: pumping out characters
-    close(fds[0]);
-    while (c < 128) {
-      write(fds[1], &c, 1);
-      c++;
-    }
-    close(fds[1]);
-    break;
-  default: // Parent: reading and printing characters
-    close(fds[1]);
-    while (read(fds[0], &c, 1)) { 
-      write(1, &c, 1);
-      read(0, &c, 1);
-    }
-    close(fds[0]);
-    write(1, "Na szevasz mi?\n", 16);
-    break;
-  }
-  return 0;
+	pipe(fds);
+	pid_t child = fork();
+	
+	switch (child) {
+	case -1: // Fork error
+		perror("fork failed");
+		return 1;
+	case 0: // Child: pumping out characters
+		close(fds[0]);
+		while (c < 128) {
+			write(fds[1], &c, 1);
+			c++;
+		}
+		close(fds[1]);
+		break;
+	default: // Parent: reading and printing characters
+		close(fds[1]);
+		while (read(fds[0], &c, 1)) { 
+			write(1, &c, 1);
+			//read(0, &c, 1);
+		}
+		close(fds[0]);
+		write(1, "\n", 1);
+		break;
+	}
+	return 0;
 }
